971ada689597642f6083996ff159f72b4488749b
Christopher Carrasco <chris@Christophers-MacBook-Pro.local>
2018-07-19 02:33:34 -0400

    detached and functioning ios simulator

---
+++ ios/Pods/Target Support Files/Pods-req-the-park-init-native/Pods-req-the-park-init-native-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_req_the_park_init_native : NSObject
+@end
+@implementation PodsDummy_Pods_req_the_park_init_native
+@end
--- src/Component/GeoLocator.js
+++ src/Component/GeoLocator.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Mapbox from '@mapbox/react-native-mapbox-gl';
-
-Mapbox.setAccessToken('pk.eyJ1IjoiY2FycmFzY29jIiwiYSI6ImNqanFpejhpNDI3aDAzcG8xcmMydWEwbDgifQ.kc6Q-adpsQFlvCCGSUvOCQ');
+// import Mapbox from '@mapbox/react-native-mapbox-gl';
+//
+// Mapbox.setAccessToken('pk.eyJ1IjoiY2FycmFzY29jIiwiYSI6ImNqanFpejhpNDI3aDAzcG8xcmMydWEwbDgifQ.kc6Q-adpsQFlvCCGSUvOCQ');
 
 
 export default class GeoLocator extends React.Component {
 
   render()  {
     return(
-      <View style={styles.container}>
-        <Mapbox.MapView
+      <View>
+        <Text>Help</Text>
+        {/* <Mapbox.MapView
             styleURL={Mapbox.StyleURL.Street}
             zoomLevel={15}
             centerCoordinate={[11.256, 43.770]}
             style={styles.container}>
-        </Mapbox.MapView>
+        </Mapbox.MapView> */}
       </View>
     )
   }
